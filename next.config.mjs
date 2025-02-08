f6896c9b3387166e27b377c439f05f62b227af31
shrutirapolu <155569386+rapoluShruti@users.noreply.github.com>
2025-02-08 23:57:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,23 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "credentialless",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
